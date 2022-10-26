baba8c2316c7c14b748004c2fcd5af2196006e01
Vinícius Vaz <viniciusvaz97@gmail.com>
2022-10-25 23:45:14 -0300

    fixed some dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['next'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "next",
-        "prettier"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 12,
+  },
+  plugins: ['react'],
+  rules: {},
 };
